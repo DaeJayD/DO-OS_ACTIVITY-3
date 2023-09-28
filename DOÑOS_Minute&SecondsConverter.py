18cc6cdbc1b9cc918767236df8a9bdf6f4611569
DaeJayD <144988177+DaeJayD@users.noreply.github.com>
2023-09-28 21:00:39 +0800

    Delete DOÑOS_Minute&SecondsConverter.py

--- DOÑOS_Minute&SecondsConverter.py
+++
@@ -1,7 +0,0 @@
-seconds = int(input("Enter an integer for seconds: "))
-
-minutes = seconds // 60
-
-seconds1 = seconds % 60
-
-print (seconds, "seconds is", minutes, "minutes and", seconds1, "seconds")
