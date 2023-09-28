d6db77ce4061417babca022e7b743ad899a371a6
DaeJayD <144988177+DaeJayD@users.noreply.github.com>
2023-09-28 21:00:59 +0800

    Add files via upload

---
+++ DOÑOS_MinSecConverter.py
@@ -0,0 +1,7 @@
+seconds = int(input("Enter an integer for seconds: "))
+
+minutes = seconds // 60
+
+seconds1 = seconds % 60
+
+print (seconds, "seconds is", minutes, "minutes and", seconds1, "seconds")
